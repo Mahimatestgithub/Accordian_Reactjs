3b59eb26f822f9701e59db059ad7920a8a4991e6
Mahima Gupta <131277652+Mahimatestgithub@users.noreply.github.com>
2024-03-27 17:27:30 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Accordian from './Components/Accordian/accordian'
+
+const App = ()=>{
+return (
+    <>
+       <Accordian/> 
+    </>
+)
+}
+export default App
---
+++ src/Components/Accordian/api.js
@@ -0,0 +1,31 @@
+export const questions=[
+    {
+        id:1,
+        question:'What is the virtual DOM?',
+        answer:'DOM stands for Document Object Model.The virtual DOM in ReactJS serves as a performance optimization by managing a lightweight copy of the real DOM for efficient rendering and updates.'
+    },
+
+    {
+        id:2,
+        question:'How do you create a React app?',
+        answer: 'Create React App by running npx create-react-app <app-name> in terminal',
+    },
+    {
+        id:3,
+        question:'What is an event in React?',
+        answer:'An event in React is a specific action, such as a user interaction like clicking a button or typing in an input field, which triggers a response or change in the application state',
+    },
+    {
+        id:4,
+        question:'What are synthetic events in React?',
+        answer:' Synthetic events in React are cross-browser wrappers around the browser native events, providing consistent behavior and properties across different browsers',
+
+    },
+    {
+        id:5,
+        question:'What are components in React?',
+        answer:'Components in React are reusable and modular building blocks that encapsulate UI elements and their functionality',
+    }
+    
+
+]
